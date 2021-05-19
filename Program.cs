397aea3e98ef20e85531ca2c5712bbff1d0006c2
Moises Silva <moises100hp@gmail.com>
2021-05-18 21:46:42 -0300

    subindo projeto

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Equails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "Maria";
+            string b = "Maria";
+
+            Console.WriteLine(a.Equals(b));
+        }
+    }
+}
